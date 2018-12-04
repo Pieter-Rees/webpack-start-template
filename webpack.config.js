7addf74713c042020031bd94d4f412464b6c5b86
Pieter Rees <pieter@pieterrees.nl>
2018-12-04 19:19:18 +0100

    added nice dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -28,6 +30,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
-    })
+    }),
+    new DashboardPlugin({ port: 4200 })
   ]
 };
