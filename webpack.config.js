44c6e062015eec565e77f516353630ffb1b731bb
Pieter Rees <pieter@pieterrees.nl>
2018-12-05 19:42:03 +0100

    fixed style loading and index.html generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const DashboardPlugin = require("webpack-dashboard/plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: { 
+  entry: {
     main: "./src/index.js"
   },
-  plugins: 
-  [
-    new DashboardPlugin({ port: 8080 }),
-  ]
-}
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+      new DashboardPlugin({ port: 8080 }),
+      new HtmlWebpackPlugin()
+    ]
+};
